99817f8977f3e4b1c0a73b8372316cddb1f7f431
Petr Hlozek <petr@ok2cqr.com>
2013-07-07 15:17:18 +0200

    added basic datamodules

---
+++ src/dData.pas
@@ -0,0 +1,26 @@
+unit dData;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil;
+
+type
+  TdmData = class(TDataModule)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  dmData: TdmData;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ src/dUtils.pas
@@ -0,0 +1,26 @@
+unit dUtils; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil; 
+
+type
+  TdmUtils = class(TDataModule)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  dmUtils: TdmUtils;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
